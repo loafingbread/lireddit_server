a333fe5fe615a7439c94b2637eceedbd0d688a90
Felix Le <felixle96@gmail.com>
2021-11-14 11:20:56 -0800

    Add user entity and resolver for login/authentication

--- src/mikro-orm.config.ts
+++ src/mikro-orm.config.ts
@@ -1,6 +1,7 @@
 import path from "path";
 import { Options } from "@mikro-orm/core";
 import { Post } from "./entities/Post";
+import { User } from "./entities/User";
 import { __prod__ } from "./constants";
 
 const config: Options = {
@@ -8,7 +9,7 @@ const config: Options = {
     path: path.join(__dirname, "migrations"), // path to the folder with migrations
     pattern: /^[\w-]+\d+\.[tj]s$/, // regex pattern for the migration files
   },
-  entities: [Post],
+  entities: [Post, User],
   dbName: "lireddit",
   user: "postgres",
   password: "postgres",
