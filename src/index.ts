47ac5f91621adf0be7ff84c86254eaa964fa2e87
Felix Le <felixle96@gmail.com>
2021-11-13 19:28:02 -0800

    Setup graphql server with test query to verify it works

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,26 @@
 import { MikroORM } from "@mikro-orm/core";
-import { Post } from "./entities/Post";
+import express from "express";
+import { ApolloServer } from "apollo-server-express";
+import { buildSchema } from "type-graphql";
 import { __prod__ } from "./constants";
 import microConfig from "./mikro-orm.config";
+import { HelloResolver } from "./resolvers/hello";
 
 const main = async () => {
   const orm = await MikroORM.init(microConfig);
   await orm.getMigrator().up();
-//   const post = orm.em.create(Post, { title: "my first post" });
-//   orm.em.persistAndFlush(post);
-  console.log("----------sql 2-----------");
-
-//   const posts = await orm.em.find(Post, {});
-//   console.log("posts: ", posts);
+  const app = express();
+  const apolloServer = new ApolloServer({
+    schema: await buildSchema({
+      resolvers: [HelloResolver],
+      validate: false,
+    }),
+  });
+  await apolloServer.start();
+  apolloServer.applyMiddleware({ app });
+  app.listen(4000, () => {
+    console.log("server started on localhost:4000");
+  });
 };
 
 main().catch((error) => {
