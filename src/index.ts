ff6dec0785976ca9921ce380bca61247fa724c61
Felix Le <felixle96@gmail.com>
2021-11-25 22:39:29 -0800

    Add session store using express-session and redis.
    * Needed to use declaration merging to add undeclared values to
    save in session object so typescript won't give compile error.

--- src/index.ts
+++ src/index.ts
@@ -2,23 +2,45 @@ import "reflect-metadata";
 import { MikroORM } from "@mikro-orm/core";
 import express from "express";
 import { ApolloServer } from "apollo-server-express";
+import redis from "redis";
+import session from "express-session";
+import connectRedis from "connect-redis";
 import { buildSchema } from "type-graphql";
 import { __prod__ } from "./constants";
 import microConfig from "./mikro-orm.config";
 import { HelloResolver } from "./resolvers/hello";
 import { PostResolver } from "./resolvers/post";
 import { UserResolver } from "./resolvers/user";
+import { MyContext } from "./types";
+
+const RedisStore = connectRedis(session);
+const redisClient = redis.createClient();
 
 const main = async () => {
   const orm = await MikroORM.init(microConfig);
   await orm.getMigrator().up();
   const app = express();
+  app.use(
+    session({
+      name: "qid",
+      store: new RedisStore({ client: redisClient, disableTouch: true }),
+      cookie: {
+        maxAge: 1000 * 60 * 60 * 24 * 365 * 10, // 10 years
+        httpOnly: true,
+        sameSite: "lax", // csrf
+        secure: __prod__, // cookie only works in https
+      },
+      saveUninitialized: true,
+      secret: "useHiddenSecretOrEnvironmentalVariables",
+      resave: false,
+    })
+  );
   const apolloServer = new ApolloServer({
     schema: await buildSchema({
       resolvers: [HelloResolver, PostResolver, UserResolver],
       validate: false,
     }),
-    context: () => ({ em: orm.em }),
+    context: (req: any, res: any): MyContext => ({ em: orm.em, req, res }),
   });
   await apolloServer.start();
   apolloServer.applyMiddleware({ app });
--- src/types.ts
+++ src/types.ts
@@ -1,6 +1,15 @@
-import { IDatabaseDriver, Connection } from "@mikro-orm/core";
-import { EntityManager } from "@mikro-orm/knex";
+import { IDatabaseDriver, Connection, EntityManager } from "@mikro-orm/core";
+import { Request, Response } from "express";
+
+declare module "express-session" {
+  export interface SessionData {
+    userId: number;
+  }
+}
+
 
 export type MyContext {
     em: EntityManager<IDatabaseDriver<Connection>>;
+    req: Request;
+    res: Response;
 }
